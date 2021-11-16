19471b0ba866353e72fccccfaf8cf882133e6b5c
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2021-11-16 03:19:55 -0800

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -9,7 +9,7 @@ from bot.helper.telegram_helper.filters import CustomFilters
 import threading
 
 
-def _watch(bot: Bot, update, isTar=False):
+def _watch(bot: Bot, update, isZip=False, isLeech=False):
     mssg = update.message.text
     message_args = mssg.split(' ')
     name_args = mssg.split('|')
@@ -20,8 +20,8 @@ def _watch(bot: Bot, update, isTar=False):
         msg = f"/{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] to mirror with youtube-dl.\n\n"
         msg += "<b>Note: Quality and custom name are optional</b>\n\nExample of quality: audio, 144, 240, 360, 480, 720, 1080, 2160."
         msg += "\n\nIf you want to use custom filename, enter it after |"
-        msg += f"\n\nExample:\n<code>/{BotCommands.WatchCommand} https://youtu.be/Pk_TthHfLeE 720 |Slam</code>\n\n"
-        msg += "This file will be downloaded in 720p quality and it's name will be <b>Slam</b>"
+        msg += f"\n\nExample:\n/{BotCommands.WatchCommand} https://youtu.be/Pk_TthHfLeE 720 |video.mp4\n\n"
+        msg += "This file will be downloaded in 720p quality and it's name will be <b>video.mp4</b>"
         sendMessage(msg, bot, update)
         return
     
@@ -44,25 +44,33 @@ def _watch(bot: Bot, update, isTar=False):
       name = ""
     
     pswd = ""
-    listener = MirrorListener(bot, update, pswd, isTar)
+    listener = MirrorListener(bot, update, pswd, isZip, isLeech=isLeech)
     ydl = YoutubeDLHelper(listener)
     threading.Thread(target=ydl.add_download,args=(link, f'{DOWNLOAD_DIR}{listener.uid}', qual, name)).start()
     sendStatusMessage(update, bot)
 
-
-def watchTar(update, context):
-    _watch(context.bot, update, True)
-
-
 def watch(update, context):
     _watch(context.bot, update)
 
+def watchZip(update, context):
+    _watch(context.bot, update, True, True)
 
-mirror_handler = CommandHandler(BotCommands.WatchCommand, watch,
+def leechWatch(update, context):
+    _watch(context.bot, update, isLeech=True)
+
+def leechWatchZip(update, context):
+    _watch(context.bot, update, True, True, True)
+
+watch_handler = CommandHandler(BotCommands.WatchCommand, watch,
                                 filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-tar_mirror_handler = CommandHandler(BotCommands.TarWatchCommand, watchTar,
+zip_watch_handler = CommandHandler(BotCommands.ZipWatchCommand, watchZip,
+                                    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+leech_watch_handler = CommandHandler(BotCommands.LeechWatchCommand, leechWatch,
+                                filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+leech_zip_watch_handler = CommandHandler(BotCommands.LeechZipWatchCommand, leechWatchZip,
                                     filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 
-
-dispatcher.add_handler(mirror_handler)
-dispatcher.add_handler(tar_mirror_handler)
+dispatcher.add_handler(watch_handler)
+dispatcher.add_handler(zip_watch_handler)
+dispatcher.add_handler(leech_watch_handler)
+dispatcher.add_handler(leech_zip_watch_handler)
