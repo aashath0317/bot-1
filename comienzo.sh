7315a3a5359971c196eaafbb76739e00d5503a2d
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2021-11-17 09:32:52 -0800

    Update comienzo.sh

--- comienzo.sh
+++ comienzo.sh
@@ -7,5 +7,6 @@ if [[ -n $ACCOUNTS_ZIP_URL ]]; then
  unzip accounts.zip -d /usr/src/app/accounts
  rm accounts.zip
 fi
-
+sudo apt install -y qbittorrent-nox
+sudo apt install aria2
 gunicorn wserver:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d --webui-port=8090 & python3 alive.py & ./aria.sh; python3 -m bot
